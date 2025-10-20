ccbf9066a0a2505aadae148cb93803af685fffbc
Ulug'bek G'ozixonov <ulugbekgozixonov@gmail.com>
2025-10-20 16:01:37 +0500

    added chance to user

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import random
 
 secret_number = random.randint(1,10)
 
-while True:
+count = 0
+
+while count<3:
     guess = int(input("Enter guess number: "))
     if guess == secret_number:
         print("Topdingiz 🎉")
         break
     else:
-        print("Topolmadingiz ❌")
+        print(f"Topolmadingiz ❌>> {count}")
+    count +=1
