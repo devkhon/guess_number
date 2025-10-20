c6442cbd9c4ebc6f06f5caef3f5824476eff76a0
Ulug'bek G'ozixonov <ulugbekgozixonov@gmail.com>
2025-10-20 15:31:50 +0500

    find guess number

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import random
 
+secret_number = random.randint(1,10)
+
+guess = int(input("Enter guess number: "))
+
+if guess == secret_number:
+    print("Topdingiz 🎉")
+else:
+    print("Topolmadingiz ❌")
