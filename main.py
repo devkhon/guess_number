aec894ec6bf973d7b53866c8deeacba5357c420d
Ulug'bek G'ozixonov <ulugbekgozixonov@gmail.com>
2025-10-22 15:30:13 +0500

    fix kode

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import random
 
 secret_number = random.randint(1,10)
-
 count = 0
 
 while count<3:
@@ -11,5 +10,5 @@ while count<3:
         break
     else:
         print(f"Topolmadingiz ❌>> {count}")
-    count = count + 1
+    count += 1
     
